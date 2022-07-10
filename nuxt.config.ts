8c2c2ee6108cac0f40ff5c51558f8aece83eebc6
fayazara <fayazara@gmail.com>
2022-07-11 01:18:34 +0530

    Remove unused modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@vueuse/nuxt'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
     content: {
         highlight: {
             theme: 'nord',
