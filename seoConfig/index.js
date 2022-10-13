9b1b19a9fdf134f976acc401c1725dff23fdc8b1
fayazara <fayazara@gmail.com>
2022-10-13 10:19:39 +0530

    Add seo metadata

---
+++ seoConfig/index.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Logspot - Opensource changelog template",
+  description:
+    "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+  og: {
+    title: "Logspot - Opensource changelog template",
+    description:
+      "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+    image: "https://logspot.vercel.app/logspot-banner.png",
+    url: "https://logspot.vercel.app",
+  },
+};
